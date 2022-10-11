c8d116ab4c6b5cee3ff99a86f4852955031d4057
SkyfallWasTaken <mahadkalam1234@gmail.com>
2022-10-11 20:49:02 +0100

    Initial for get versions and ranges

--- src/get_versions_and_ranges.rs
+++ src/get_versions_and_ranges.rs
@@ -6,7 +6,7 @@ use tokio::fs::read_to_string;
 use tokio::task;
 
 #[derive(Deserialize, Debug)]
-struct Empty;
+struct Empty {}
 
 #[derive(Deserialize, Debug)]
 struct Dependency {
@@ -17,8 +17,10 @@ pub async fn invoke() -> Result<()> {
     let package_names: Vec<String> =
         serde_json::from_str(&read_to_string("out/package_names.json").await?)?;
 
+    let mut data = Vec::new();
+
     for name in package_names {
-        task::spawn(async move {
+        let dependency = task::spawn(async move {
             let client = get_client();
             let dependency: Dependency = client
                 .get(format!("https://skimdb.npmjs.com/registry/{name}"))
@@ -29,10 +31,16 @@ pub async fn invoke() -> Result<()> {
                 .await
                 .unwrap();
 
-            dbg!(dependency);
+            dependency
         })
         .await?;
+
+        for (version, _) in dependency.versions {
+            data.push(version)
+        }
     }
+
+    dbg!(data.dedup());
 
     Ok(())
 }
